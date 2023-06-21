30f5151740741e1f5d47b1dd68dfc505293250b6
Kamrul Alam <kamrul.alam1983@gmail.com>
2023-06-21 16:12:43 +0600

    Array6

--- src/ArrayPackage/Array6.java
+++ src/ArrayPackage/Array6.java
@@ -11,6 +11,9 @@ public class Array6 {
 		System.out.println("Please enter elements of Matrix: ");
 		
 		int[][] A = new int[3][3];
+		int diagSum = 0;
+		int uppSum = 0;
+		int lowSum = 0;
 		
 		//Taking input for Array A[][]
 		for(int row = 0; row < 3; row++) {
@@ -36,6 +39,30 @@ public class Array6 {
 		}
 		
 		//Summation of DIAGONAL [0][0] + [1][1] + [2][2] Elements
-		
+		for(int row = 0; row < 3; row++) {
+			
+			for(int col = 0; col < 3; col++) {
+				
+				if(row == col) {
+					
+					diagSum = diagSum + A[row][col];
+				}
+				
+				if (row < col) {
+					
+					//System.out.println(A[row][col]);
+					uppSum = uppSum + A[row][col];
+				}
+				
+				if (row > col) {
+					
+					//System.out.println(A[row][col]);
+					lowSum = lowSum + A[row][col];
+				}
+			}
+		}
+		System.out.println("Sum of Diagonal values: " + diagSum);
+		System.out.println("Sum of Upper Triangle: " + uppSum);
+		System.out.println("Sum of Lower Triangle: " + lowSum);
 	}
 }
